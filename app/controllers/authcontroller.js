b32d742e933614727864ee88e58c02ed9e114b6a
Roger Rodriguez <evolpirate@gmail.com>
2018-07-18 19:57:51 -0400

    initial commit

---
+++ app/controllers/authcontroller.js
@@ -0,0 +1,28 @@
+var exports = module.exports = {}
+
+
+exports.signup = function(req,res){
+
+	res.render('signup');
+
+}
+
+exports.signin = function(req,res){
+
+	res.render('signin');
+
+}
+
+exports.dashboard = function(req,res){
+  console.log(req.user);
+	res.render('dashboard',{user: req.user});
+
+}
+
+exports.logout = function(req,res){
+
+  req.session.destroy(function(err) {
+  res.redirect('/');
+  });
+
+}
